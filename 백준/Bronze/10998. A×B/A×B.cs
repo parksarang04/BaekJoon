566be42ad5364fbc57d4018f592939ebce1a3f88
parksarang04 <162421036+parksarang04@users.noreply.github.com>
2025-08-18 20:28:34 +0900

    [Bronze V] Title: A×B, Time: 48 ms, Memory: 5496 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.cs
@@ -0,0 +1,11 @@
+using System;
+class Program
+{
+    static void Main()
+    {
+        string[] input = Console.ReadLine().Split();
+        int A = int.Parse(input[0]);
+        int B = int.Parse(input[1]);
+        Console.WriteLine(A * B);
+    }
+}
