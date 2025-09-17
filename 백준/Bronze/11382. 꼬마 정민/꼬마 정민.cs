5516dbafa00c76948400c72b8968e61dd6e0cf16
parksarang04 <162421036+parksarang04@users.noreply.github.com>
2025-09-17 15:58:18 +0900

    [Bronze V] Title: 꼬마 정민, Time: 48 ms, Memory: 5516 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cs
@@ -0,0 +1,6 @@
+string[] input = Console.ReadLine().Split();
+long a = Convert.ToInt64(input[0]);
+long b = Convert.ToInt64(input[1]);
+long c = Convert.ToInt64(input[2]);
+
+Console.WriteLine(a+b+c);
