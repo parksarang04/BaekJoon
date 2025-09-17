d2c7b110f4fda2668f79cbb024363bccd8948650
parksarang04 <162421036+parksarang04@users.noreply.github.com>
2025-09-17 15:18:41 +0900

    [Bronze V] Title: 사칙연산, Time: 48 ms, Memory: 5536 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cs
@@ -0,0 +1,9 @@
+string[] input = Console.ReadLine().Split();
+int a= Convert.ToInt32(input[0]);
+int b= Convert.ToInt32(input[1]);
+
+Console.WriteLine(a+b);
+Console.WriteLine(a-b);
+Console.WriteLine(a*b);
+Console.WriteLine(a/b);
+Console.WriteLine(a%b);
